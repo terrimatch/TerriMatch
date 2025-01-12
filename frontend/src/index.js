fece6e329f74d79f8e06dd42c9e2f3a080c0e570
terrimatch <terrimatch.contact@gmail.com>
2025-01-12 14:59:41 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import TelegramWebApp from './TelegramWebApp';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <TelegramWebApp />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
